817657a3aa23239515f9adf820c70b08a58eebfc
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2020-03-01 16:33:50 +0700

    add page api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server-express';
+import pageApis from './routes/apis/page'
 import express from 'express';
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
@@ -35,13 +36,15 @@ app.use(bodyParser.json())
 // const kitty = new Cat({ name: 'Zildjian' });
 // kitty.save().then(() => console.log('meow'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/index.html'))
+// })
 
 app.get('/', (req, res) => {
   res.send('<h1> Hello world </h1>')
 })
+
+app.use('/api/pages', pageApis)
 
 app.listen({ port }, () => {
   // console.log(`🚀  Server ready at http://localhost:4000${server.graphqlPath}`)
--- src/routes/apis/page.js
+++ src/routes/apis/page.js
@@ -1,12 +1,51 @@
-import express from express
-import Page from '../../models/Page'
+import express from 'express'
+import Page from '../../mongodb/models/Page'
 
-const router = express.router()
+const router = express.Router()
 
+
+// get all
 router.get('/', async (req, res, next) => {
   try {
-    const page = await Post.find()
-    console.log(page)
+    const pages = await Page.find()
+    res.status(200).json(pages)
+    console.log(pages)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.post('/add', async (req, res, next) => {
+  try {
+    const { name, title, description, mediaUrl } = req.body
+    const page = await new Page({
+      name,
+      title,
+      mediaUrl,
+      description
+    }).save()
+    res.status(201).json(page)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.put('/update/:_id', async (req, res, next) => {
+  console.log('req', req)
+  try {
+    const { _id } = req.params
+    await Page.findByIdAndUpdate({ _id }, { ...req.body })
+    res.status(203).send('Page updated')
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.delete('/:_id', async (req, res, next) => {
+  try {
+    const { _id } = req.params
+    await Page.findByIdAndRemove({ _id })
+    res.status(204).send('Page delete')
   } catch (err) {
     console.log(err)
   }
