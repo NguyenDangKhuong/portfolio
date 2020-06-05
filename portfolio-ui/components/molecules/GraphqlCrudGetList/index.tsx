dc12a2af0deb2a031960bbe7505fc2366258dbc4
nguyendangkhuong96 <nguyendang.khuong@asoview.vn>
2020-06-05 13:10:20 +0700

    detail crud

--- portfolio-ui/components/molecules/GraphqlCrudGetList/index.tsx
+++ portfolio-ui/components/molecules/GraphqlCrudGetList/index.tsx
@@ -1,16 +1,94 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import { Tab } from 'semantic-ui-react'
+import { Table,  Image, Header} from 'semantic-ui-react'
 
 function GraphqlCrudGetList () { 
-  const panes = [
-    { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-    { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-    { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-  ]
+
 
   return (
-    <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
+    <Table striped>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Avatar</Table.HeaderCell>
+        <Table.HeaderCell>Username</Table.HeaderCell>
+        <Table.HeaderCell>E-mail</Table.HeaderCell>
+        <Table.HeaderCell>Image</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>
+          <Header as='h4' image>
+            <Image src='https://react.semantic-ui.com/images/avatar/small/lena.png' rounded size='mini' />
+            <Header.Content>
+              Lena
+              <Header.Subheader>Human Resources</Header.Subheader>
+            </Header.Content>
+          </Header>
+        </Table.Cell>
+        <Table.Cell>September 14, 2013</Table.Cell>
+        <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+        <Table.Cell>No</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>
+        <Header as='h4' image>
+            <Image src='https://react.semantic-ui.com/images/avatar/small/lena.png' rounded size='mini' />
+            <Header.Content>
+              Jamie Harington
+              <Header.Subheader>Human Resources</Header.Subheader>
+            </Header.Content>
+          </Header>
+        </Table.Cell>
+        <Table.Cell>January 11, 2014</Table.Cell>
+        <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+        <Table.Cell>Yes</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>
+        <Header as='h4' image>
+            <Image src='https://react.semantic-ui.com/images/avatar/small/lindsay.png' rounded size='mini' />
+            <Header.Content>
+              Jill Lewis
+              <Header.Subheader>Human Resources</Header.Subheader>
+            </Header.Content>
+          </Header>
+        </Table.Cell>
+        <Table.Cell>January 11, 2014</Table.Cell>
+        <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+        <Table.Cell>Yes</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>
+        <Header as='h4' image>
+            <Image src='https://react.semantic-ui.com/images/avatar/small/mark.png' rounded size='mini' />
+            <Header.Content>
+              John Lilki
+              <Header.Subheader>Human Resources</Header.Subheader>
+            </Header.Content>
+          </Header>
+        </Table.Cell>
+        <Table.Cell>January 11, 2014</Table.Cell>
+        <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+        <Table.Cell>Yes</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>
+        <Header as='h4' image>
+            <Image src='https://react.semantic-ui.com/images/avatar/small/matthew.png' rounded size='mini' />
+            <Header.Content>
+              John Lilki
+              <Header.Subheader>Human Resources</Header.Subheader>
+            </Header.Content>
+          </Header>
+        </Table.Cell>
+        <Table.Cell>January 11, 2014</Table.Cell>
+        <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+        <Table.Cell>Yes</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
   )
 }
 
--- portfolio-ui/components/organisms/Crud/GraphqlCrud/index.tsx
+++ portfolio-ui/components/organisms/Crud/GraphqlCrud/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 import { Tab } from 'semantic-ui-react'
+import GraphqlCrudGetList from '../../../molecules/GraphqlCrudGetList'
 
 function GraphqlCrud () { 
   const panes = [
-    { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+    { menuItem: 'Tab 1', render: () => <Tab.Pane><GraphqlCrudGetList /></Tab.Pane> },
     { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
     { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
   ]
